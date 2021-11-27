7b69e394b3c6b96045964910a8d91d0cdb08ef7a
Forairaaaaa <7525376+forairaaaaa@user.noreply.gitee.com>
2021-11-27 10:12:55 +0800

    add test sample

--- TestSamples/PlatformIO/E-pico/src/main.cpp
+++ TestSamples/PlatformIO/E-pico/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+
+U8G2_SH1107_128X80_F_HW_I2C u8g2(U8G2_R1, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/ 21);
+
+int i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+    // U8g2 setup
+    u8g2.begin();
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+    u8g2.drawStr(0,10,"E-pico");
+    u8g2.sendBuffer();
+
+    // Serial
+    Serial.begin(115200);
+
+    // Test led
+    pinMode(2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // // Serial print test
+    Serial.print("Hi, ");
+    Serial.println(i++);
+
+    // Test led
+    digitalWrite(2, !digitalRead(2));
+
+    delay(1000);
 }
